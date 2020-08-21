67a21810fca4216d2c4a58349f5664fb1f45fb39
Tadas <tdavidsonas88@gmail.com>
2020-08-21 13:31:35 +0300

    Subscriber - many events handled by one subscriber

---
+++ src/EventSubscriber/VideoCreatedSubscriber.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\EventSubscriber;
+
+use Symfony\Component\EventDispatcher\EventSubscriberInterface;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpKernel\Event\ResponseEvent;
+use Symfony\Component\HttpKernel\KernelEvents;
+
+class VideoCreatedSubscriber implements EventSubscriberInterface
+{
+    public function onVideoCreatedEvent($event)
+    {
+        dump($event->video->title);
+    }
+
+    public function onKernelResponse(ResponseEvent $event)
+    {
+        $response = new Response('dupa');
+        $event->setResponse($response);
+    }
+
+    public static function getSubscribedEvents()
+    {
+        return [
+            'video.created.event' => 'onVideoCreatedEvent',
+            KernelEvents::RESPONSE => 'onKernelResponse',
+        ];
+    }
+}
