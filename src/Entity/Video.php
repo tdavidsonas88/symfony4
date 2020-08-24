1d617c25b9463864389fcdce4c90faf5d106a580
Tadas <tdavidsonas88@gmail.com>
2020-08-24 15:57:52 +0300

    file upload button and bootstrap css added

--- src/Entity/Video.php
+++ src/Entity/Video.php
@@ -46,6 +46,16 @@ class Video extends File
      */
     private $created_at;
 
+    /**
+     * @ORM\Column(type="string", length=255)
+     * @Assert\File(
+     *     maxSize="1024k",
+     *     mimeTypes={"video/mp4", "application/pdf", "application/x-pdf"},
+     *     mimeTypesMessage="Please upload a valid video"
+     * )
+     */
+    private $file;
+
     public function getTitle(): ?string
     {
         return $this->title;
@@ -102,6 +112,18 @@ class Video extends File
     public function setCreatedAt(\DateTimeInterface $created_at): self
     {
         $this->created_at = $created_at;
+
+        return $this;
+    }
+
+    public function getFile(): ?string
+    {
+        return $this->file;
+    }
+
+    public function setFile(string $file): self
+    {
+        $this->file = $file;
 
         return $this;
     }
