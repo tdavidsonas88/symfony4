8444a69389bd855d5ec95692c4e3e1206e3481f6
Tadas <tdavidsonas88@gmail.com>
2020-08-24 19:27:39 +0300

    unique email annotation

--- src/Entity/SecurityUser.php
+++ src/Entity/SecurityUser.php
@@ -6,11 +6,13 @@ use App\Repository\SecurityUserRepository;
 use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Bridge\Doctrine\Validator\Constraints\UniqueEntity;
 use Symfony\Component\Security\Core\User\UserInterface;
 use Symfony\Component\Validator\Constraints as Assert;
 
 /**
  * @ORM\Entity(repositoryClass=SecurityUserRepository::class)
+ * @UniqueEntity("email")
  */
 class SecurityUser implements UserInterface
 {
